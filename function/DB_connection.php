92a2b76249c7cd3580912c993764f551818ee8c2
lemduc <lemduc@hotmail.com>
2019-12-23 07:13:39 +0700

    Sua BTL

--- function/DB_connection.php
+++ function/DB_connection.php
@@ -1,9 +1,19 @@
 <?php
-DEFINE ('DB_USER', 'root');
-DEFINE ('DB_PASSWORD', '');
-DEFINE ('DB_HOST', 'localhost');
-DEFINE ('DB_NAME', 'QLDiemSV');
+function getConnect() {
+    DEFINE ('DB_USER', 'root');
+    DEFINE ('DB_PASSWORD', '');
+    DEFINE ('DB_HOST', 'localhost');
+    DEFINE ('DB_NAME', 'QLDiemSV');
+    
+    $dbcon = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    mysqli_set_charset($dbcon, 'utf8');
 
-$dbcon = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-mysqli_set_charset($dbcon, 'utf8');
+    // Kiểm tra lỗi kết nối
+    if ($dbcon->connect_error) {
+        die("Lỗi kết nối: " . mysqli_connect_error());
+        return null;
+    }
+    echo "Kết nối thành công";
+    return $dbcon;
+}
 ?>
--- index.php
+++ index.php
@@ -10,6 +10,10 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
+    <?php
+      include "function/DB_connection.php";
+      getConnect();
+    ?>
       <div></div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
