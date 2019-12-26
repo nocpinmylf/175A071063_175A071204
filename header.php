c12aae3bd46d2625ba3a39230f419e7b4d625e21
hoabt <hoabt72@wru.vn>
2019-12-26 21:08:01 +0700

    sửa file header.php

--- header.php
+++ header.php
@@ -26,12 +26,12 @@
                 <span class="sr-only">(current)</span>
               </a>
             </li>
-            <li class="nav-item">
+            <!-- <li class="nav-item">
               <a class="nav-link" href="#">Features</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">Pricing</a>
-            </li>
+            </li> -->
           </ul>
           <ul class="navbar-nav ml-auto nav-flex-icons">
             <!-- <li class="nav-item">
