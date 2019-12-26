4ce68f917d62f0b16ddd06ded30e2cd2752e63e8
hoabt <hoabt72@wru.vn>
2019-12-26 21:10:01 +0700

    sửa file header.php lần 2

--- header.php
+++ header.php
@@ -39,15 +39,7 @@
                 <i class="fab fa-google-plus-g"></i>
               </a>
             </li> -->
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">👨‍🎓</a>
-              <div class="dropdown-menu dropdown-menu-right dropdown-default"
-                aria-labelledby="navbarDropdownMenuLink-333">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <a class="dropdown-item" href="#">Something else here</a>
-              </div>
-            </li>
+            
           </ul>
         </div>
       </nav>
